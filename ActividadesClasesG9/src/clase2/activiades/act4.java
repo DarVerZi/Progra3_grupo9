0e7c30a7bf8473d92dfaaf7c3ee1464d9def5ed4
JulianEtchebarne <julianetchebarne@hotmail.com>
2024-09-27 20:05:15 -0300

    Codigo clase 2

--- ActividadesClasesG9/src/clase2/activiades/act4.java
+++ ActividadesClasesG9/src/clase2/activiades/act4.java
@@ -1,4 +1,15 @@
 package clase2.activiades;
 
+import clase2.algoritmos.MergeSort;
+
 public class act4 {
+
+public static void main(String[] args) {    int[] listaDesordenada2 = {34, 7, 23, 32, 5, 62};
+    MergeSort.mergeSort(listaDesordenada2);
+    System.out.println("Lista ordenada:");
+    for (int i = 0; i < listaDesordenada2.length; i++) {
+        System.out.print(listaDesordenada2[i] + " ");
+    }}
+
+
 }
